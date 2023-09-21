e253aeb439588c66e9d526337c3d5dd13520f7d6
Matheus Possenti <matheus.possenti24@gmail.com>
2023-09-21 11:23:24 -0300

    Adicionado Componente Avatar e Responsividade

--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { PencilLine } from "@phosphor-icons/react";
 
 import styles from "./Sidebar.module.css";
+import { Avatar } from "./Avatar";
 
 export function Sidebar() {
   return (
@@ -10,10 +11,7 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1485575301924-6891ef935dcd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGZsZWV0fGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/matheuspossenti.png"
-        />
+        <Avatar hasBorder src="https://github.com/matheuspossenti.png" />
 
         <strong>Matheus Possenti</strong>
         <span>Web Developer</span>
